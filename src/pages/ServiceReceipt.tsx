9393a7d55b3808ae20c8e49a8d6dc9fe6a96fdd3
Attila Reterics <reterics.attila@gmail.com>
2024-10-02 20:47:10 +0200

    signature pad added

--- src/pages/ServiceReceipt.tsx
+++ src/pages/ServiceReceipt.tsx
@@ -1,8 +1,9 @@
 import {PageHead} from "../components/elements/PageHead.tsx";
 import {BsFillPlusCircleFill} from "react-icons/bs";
-import {useContext, useState} from "react";
+import {useContext, useEffect, useRef, useState} from "react";
 import {FirebaseContext} from "../firebase/FirebaseContext.ts";
 import {useTranslation} from "react-i18next";
+import SignaturePad from "react-signature-pad-wrapper";
 
 
 function ServiceReceipt() {
@@ -25,6 +26,20 @@ function ServiceReceipt() {
 
     const index = 1;
     const date = new Date();
+
+    const signaturePadRef = useRef<SignaturePad>(null);
+
+    useEffect(() => {
+        if (signaturePadRef.current) {
+            const signaturePad = signaturePadRef.current;
+            signaturePad.clear();
+            signaturePad.isEmpty();
+
+            signaturePad.minWidth = 5;
+            signaturePad.maxWidth = 10;
+            signaturePad.penColor = 'rgb(66, 133, 244)';
+        }
+    }, []);
 
     return (
         <>
@@ -62,6 +77,9 @@ function ServiceReceipt() {
                 <p>Appendix</p>
 
                 Signature box
+                <SignaturePad ref={signaturePadRef}
+                              redrawOnResize
+                              options={{minWidth: 5, maxWidth: 10, penColor: 'rgb(66, 133, 244)'}} />
 
                 Download PDF button
             </div>
