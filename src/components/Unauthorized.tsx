ee5f5a44d0b05455c152caff391568cdcec1f5b2
Attila Reterics <reterics.attila@gmail.com>
2024-10-27 20:50:41 +0100

    logout added to unauth page

--- src/components/Unauthorized.tsx
+++ src/components/Unauthorized.tsx
@@ -21,6 +21,13 @@ const UnauthorizedComponent = () => {
                 <div className={"font-normal text-xl mt-2 text-center items-center"}>
                     {t('401 Unauthorized - Your privileges has been revoked')}
                 </div>
+                <button
+                    onClick={() => {
+                        SignOut()
+                    }}
+                    className="w-36 mt-2 text-white bg-gray-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
+                    {t('Logout')}
+                </button>
             </div>
 
         </div>
